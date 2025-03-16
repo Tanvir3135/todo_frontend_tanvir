cc1dab709ab3a3aadca16e2e24efc3d064a9aa6a
Tanvir3135 <t3ahta00@students.oamk.fi>
2025-03-16 13:26:36 +0545

    init

---
+++ js/class/Todos.js
@@ -0,0 +1,88 @@
+import { Task } from "./Task.js";
+
+class Todos {
+  #tasks = [];
+  #backend_url = "";
+
+  constructor(backend_url) {
+    this.#backend_url = backend_url;
+  }
+
+  getTasks = () => {
+    return new Promise(async (resolve, reject) => {
+      fetch(this.#backend_url)
+        .then((response) => response.json())
+        .then(
+          (json) => {
+            this.#readJson(json);
+            resolve(this.#tasks);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  };
+
+  #readJson = (tasksAsJson) => {
+    tasksAsJson.forEach((node) => {
+      const task = new Task(node.id, node.description);
+      this.#tasks.push(task);
+    });
+  };
+
+  #addToArray = (id, text) => {
+    const task = new Task(id, text);
+    this.#tasks.push(task);
+    return task;
+  };
+
+  #removeFromArray = (id) => {
+    const arrayWithoutRemoved = this.#tasks.filter((task) => {
+      return task.getId() !== id;
+    });
+    this.#tasks = arrayWithoutRemoved;
+  };
+
+  addTask = (text) => {
+    return new Promise(async (resolve, reject) => {
+      fetch(this.#backend_url + "/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ description: text }),
+      })
+        .then((response) => response.json())
+        .then(
+          (json) => {
+            const task = this.#addToArray(json.id, text);
+            resolve(task);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  };
+
+  removeTask = (id) => {
+    return new Promise(async (resolve, reject) => {
+      fetch(this.#backend_url + "/delete/" + id, {
+        method: "DELETE",
+      })
+        .then((response) => response.json())
+        .then(
+          (json) => {
+            this.#removeFromArray(id);
+            resolve(json.id);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+    });
+  };
+}
+
+export { Todos };
---
+++ js/index.js
@@ -0,0 +1,73 @@
+import { Todos } from "./class/Todos.js";
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const BACKEND_ROOT_URL = "http://localhost:3001";
+const todos = new Todos(BACKEND_ROOT_URL);
+
+input.disabled = true;
+
+const renderSpan = (li, text) => {
+  const span = li.appendChild(document.createElement("span"));
+  span.innerHTML = text;
+};
+const renderLink = (li, id) => {
+  const a = li.appendChild(document.createElement("a"));
+  a.innerHTML = '<i class="bi bi-trash"></i>';
+  a.setAttribute("style", "float: right");
+  a.addEventListener("click", () => {
+    todos
+      .removeTask(id)
+      .then((id) => {
+        const li_to_remove = document.querySelector(`[data-key="${id}"]`);
+        if (li_to_remove) {
+          list.removeChild(li_to_remove);
+        }
+      })
+      .catch((error) => {
+        alert("Error removing task", error.message);
+      });
+  });
+};
+const renderTask = (task) => {
+  const li = document.createElement("li");
+  li.setAttribute("class", "list-group-item");
+  li.setAttribute("data-key", task.getId()); // Add this line to set the data-key
+  renderLink(li, task.getId());
+  renderSpan(li, task.getText());
+  list.append(li);
+};
+
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    const task = input.value.trim();
+    if (task !== "") {
+      todos
+        .addTask(task)
+        .then((task) => {
+          renderTask(task);
+          input.value = "";
+          input.focus();
+        })
+        .catch((error) => {
+          alert("Error saving task", error.message);
+        });
+    }
+  }
+});
+
+const getTasks = () => {
+  todos
+    .getTasks()
+    .then((tasks) => {
+      tasks.forEach((task) => {
+        renderTask(task);
+      });
+      input.disabled = false;
+    })
+    .catch((error) => {
+      alert("Error retrieving tasks", error.message);
+    });
+};
+
+getTasks();
